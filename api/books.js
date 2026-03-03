b9718b2a59eb777cf44768b0017e15a2795d3771
houdaifa <houdaifazaidi04@gmail.com>
2026-03-03 09:01:32 +0000

    revert

--- api/books.js
+++ api/books.js
@@ -13,12 +13,12 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-    next()
-})
+// app.use(function(req, res, next){
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+//     next()
+// })
 
 
 // Connect to MongoDB
